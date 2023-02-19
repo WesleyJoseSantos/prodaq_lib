dd11f8723698a8e36cb11a45695fcc4c07eecc28
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-02-18 23:41:07 -0300

    test: mock structs

--- prodaq_config/test/include/prodaq_mocks.h
+++ prodaq_config/test/include/prodaq_mocks.h
@@ -14,23 +14,37 @@
 
 #define PRODAQ_JSON_PATH JSON_PATH "/"
 
-#define MOCK_RESPONSE_MESSAGE_JSON PRODAQ_JSON_PATH "response_message.json"
-#define MOCK_RESPONSE_MESSAGE_DATA_JSON PRODAQ_JSON_PATH "response_data.json"
+#define MOCK_REQUEST_MESSAGE_DATA_JSON PRODAQ_JSON_PATH "request_data.json"
+#define MOCK_REQUEST_MESSAGE_JSON PRODAQ_JSON_PATH "request_message.json"
 
-#define MOCK_RESPONSE \
-    {                 \
-        .err = 0,     \
+#define MOCK_RESPONSE_MESSAGE_DATA_JSON PRODAQ_JSON_PATH "response_data.json"
+#define MOCK_RESPONSE_MESSAGE_JSON PRODAQ_JSON_PATH "response_message.json"
+
+#define MOCK_RESPONSE                \
+    {                                \
+        .err = 0,                    \
+        .message = "Prodaq Message", \
     }
 
-#define MOCK_RESPONSE_MESSAGE                \
-    {                                        \
-        .id = MSG_ID_RESPONSE,               \
-        .data = {                            \
-            .response = {                    \
-                .err = PRODAQ_OK,            \
-                .message = "Prodaq Message", \
-            }                                \
-        }                                    \
+#define MOCK_RESPONSE_MESSAGE         \
+    {                                 \
+        .id = MSG_ID_RESPONSE,        \
+        .data = {                     \
+            .response = MOCK_RESPONSE \
+        }                             \
+    }
+
+#define MOCK_REQUEST        \
+    {                       \
+        .id = 1, .type = 2 \
+    }
+
+#define MOCK_REQUEST_MESSAGE        \
+    {                               \
+        .id = 127,                  \
+        .data = {                   \
+            .request = MOCK_REQUEST \
+        }                           \
     }
 
 #endif //!__PRODAQ_MOCKS__H__
