b36ddf55e4549b60d75867460602a9e8be2d171d
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-02-18 23:41:49 -0300

    feat: add tests prototyes

--- prodaq_config/test/include/prodaq_tests.h
+++ prodaq_config/test/include/prodaq_tests.h
@@ -18,9 +18,14 @@ void prodaq_fm_load_test(void);
 void message_data_hardware_type_from_json_test(void);
 void message_data_protocol_type_from_json_test(void);
 void message_data_device_type_from_json_test(void);
+void message_data_request_type_from_json_test(void);
+void message_data_response_type_from_json_test(void);
+
 void message_data_hardware_type_to_json_test(void);
 void message_data_protocol_type_to_json_test(void);
 void message_data_device_type_to_json_test(void);
+void message_data_request_type_to_json_test(void);
+void message_data_response_type_to_json_test(void);
 
 void message_wifi_from_json_test(void);
 void message_wifi_to_json_test(void);
@@ -28,6 +33,10 @@ void message_ethernet_from_json_test(void);
 void message_ethernet_to_json_test(void);
 void message_uart_from_json_test(void);
 void message_uart_to_json_test(void);
+
+void request_message_from_json_test(void);
+void request_message_to_json_test(void);
+
 void response_message_from_json_test(void);
 void response_message_to_json_test(void);
 
@@ -37,6 +46,8 @@ void message_http_from_json_test(void);
 void message_http_to_json_test(void);
 void message_mqtt_from_json_test(void);
 void message_mqtt_to_json_test(void);
+void message_request_from_json_test(void);
+void message_request_to_json_test(void);
 void message_response_from_json_test(void);
 void message_response_to_json_test(void);
 
