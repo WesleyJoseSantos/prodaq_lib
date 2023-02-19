b076f17539308e292f517720ebf45c2a257eb10d
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-02-18 23:11:20 -0300

    feat: add request_message_t to message_data_t struct

--- prodaq_config/include/prodaq_message_data.h
+++ prodaq_config/include/prodaq_message_data.h
@@ -15,6 +15,7 @@
 #include "hardware_message.h"
 #include "protocol_message.h"
 #include "device_message.h"
+#include "prodaq_request.h"
 #include "prodaq_response.h"
 #include "prodaq_json.h"
 
@@ -28,7 +29,8 @@ typedef enum
     MSG_ID_HARDWARE_CONFIG = 1, // Message id for hardware configuration
     MSG_ID_PROTOCOL_CONFIG = 2, // Message id for protocol configuration
     MSG_ID_DEVICE_CONFIG = 3,   // Message id for device configuration
-    MSG_ID_RESPONSE = 255,      // Message id that responds an request
+    MSG_ID_REQUEST = 127,       // Message id to request an resource
+    MSG_ID_RESPONSE = 255,      // Message id that responds an configuration message
 } message_id_t;
 
 /**
@@ -40,7 +42,8 @@ typedef union
     hardware_message_t hardware; // Hardware configuration data
     protocol_message_t protocol; // Protocol configuration data
     device_message_t device;     // Device configuration data
-    response_message_t response; // Message that responds an request
+    request_message_t request;   // Message to request an resource
+    response_message_t response; // Message that responds an configuration message
 } message_data_t;
 
 /**
