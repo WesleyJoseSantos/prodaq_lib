29231b5d0c558eda29efd02667cf3771e0001444
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-02-25 07:30:39 -0300

    feat: config changes

--- prodaq_config/include/prodaq_config.h
+++ prodaq_config/include/prodaq_config.h
@@ -12,33 +12,34 @@
 #ifndef __PRODAQ_APP_CONFIG__H__
 #define __PRODAQ_APP_CONFIG__H__
 
-// #define PRODAQ_TARGET_WIN
-// #define PDODAQ_TARGET_IDF
-#define PRODAQ_TARGET_INO
-
-#ifdef  PRODAQ_TARGET_WIN
-#define PRODAQ_TARGET_SELECTED
-#endif  //PRODAQ_TARGET_FM
-
-#ifdef  PRODAQ_TARGET_INO
-#define PRODAQ_TARGET_SELECTED
-#endif  //PRODAQ_TARGET_INO
-
-#ifdef  PDODAQ_TARGET_IDF
-#define PRODAQ_TARGET_SELECTED
-#endif  //PRODAQ_TARGET_FM
-
-#if !defined(PRODAQ_TARGET_SELECTED)
-#error "Target is not selected"
+#if !defined(__WINDOWS__) && (defined(WIN32) || defined(WIN64) || defined(_MSC_VER) || defined(_WIN32))
+#define __WINDOWS__
 #endif
 
-#define PRODAQ_FILENAME_WIFI "wifi.bin"
-#define PRODAQ_FILENAME_ETHERNET "ethernet.bin"
-#define PRODAQ_FILENAME_UART "uart.bin"
-#define PRODAQ_FILENAME_NTP "ntp.bin"
-#define PRODAQ_FILENAME_HTTP "http.bin"
-#define PRODAQ_FILENAME_MQTT "mqtt.bin"
+#if !defined(__LINUX__) && defined __linux__
+#define __LINUX__
+#endif
 
+#if !defined(__ARDUINO__) && (defined(__AVR__) || defined(ESP8266))
+#define __ARDUINO__
+#endif
+
+#if !defined(__IDF__) && defined(IDF_VER)
+#define __IDF__
+#endif
+
+#if !defined(__WINDOWS__) && !defined(__LINUX__) && !defined(__ARDUINO__) && !defined(__IDF__)
+#error "System not supported!"
+#endif
+
+#define PRODAQ_FILENAME_WIFI "/wifi.bin"
+#define PRODAQ_FILENAME_ETHERNET "/ethernet.bin"
+#define PRODAQ_FILENAME_UART "/uart.bin"
+#define PRODAQ_FILENAME_NTP "/ntp.bin"
+#define PRODAQ_FILENAME_HTTP "/http.bin"
+#define PRODAQ_FILENAME_MQTT "/mqtt.bin"
+
+#define PRODAQ_SERVER_FORCE_FM
 #define PRODAQ_SERVER_PORT 80
 #define PRODAQ_SERVER_URL "/prodaq"
 #define PRODAQ_SERVER_URL_SIZE 64
