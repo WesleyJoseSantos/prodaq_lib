f953f4344e6c4ad6f94b9627d5969ed8e3a1c1d8
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-02-27 22:07:43 -0300

    feat: implement tcp status serialization

---
+++ prodaq_config/src/network/tcp_status.c
@@ -0,0 +1,28 @@
+/**
+ * @file tcp_status.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-27
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "network/tcp_status.h"
+
+prodaq_err_t tcp_status_from_json(PRODAQ_FROM_JSON_PARAMETERS(tcp_status_t))
+{
+    PRODAQ_JSON_CHECK_PARAMETERS();
+    PRODAQ_FROM_JSON_NUMBER_ARRAY(ip);
+    PRODAQ_FROM_JSON_BOOLEAN(connected);
+    return PRODAQ_OK;
+}
+
+prodaq_err_t tcp_status_to_json(PRODAQ_TO_JSON_PARAMETERS(tcp_status_t))
+{
+    PRODAQ_JSON_CHECK_PARAMETERS();
+    PRODAQ_TO_JSON_NUMBER_ARRAY(ip);
+    PRODAQ_TO_JSON_BOOLEAN(connected);
+    return PRODAQ_OK;
+}
