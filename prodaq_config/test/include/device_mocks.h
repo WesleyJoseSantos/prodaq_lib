6d0eab76f7309cb21bf37251424c6a6bfbe9fe8a
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-03-01 22:41:02 -0300

    test: add gateway and datalogger mocks

--- prodaq_config/test/include/device_mocks.h
+++ prodaq_config/test/include/device_mocks.h
@@ -12,10 +12,6 @@
 #ifndef __DEVICE_MOCKS__H__
 #define __DEVICE_MOCKS__H__
 
-#include <stdio.h>
-
-#include "json.h"
-
 #define DEVICE_JSON_PATH JSON_PATH "/device/"
 
 #define MOCK_SENSOR_CONFIG_JSON DEVICE_JSON_PATH "sensor_config.json"
@@ -40,18 +36,64 @@
         .hardware = SENSOR_HW_DHT22                \
     }
 
-#define MOCK_SENSOR_MESSAGE                                \
-    {                                                      \
-        .id = MSG_ID_DEVICE_CONFIG,                        \
-        .data = {                                          \
-            .device = {                                    \
-                .id = 1234,                                \
-                .interval = 60,                            \
-                .resolution = 10,                          \
-                .samples = 5,                              \
-                .data_type = SENSOR_DATA_TYPE_TEMPERATURE, \
-                .hardware = SENSOR_HW_DHT22}               \
-        }                                                  \
+#define MOCK_SENSOR_MESSAGE                        \
+    {                                              \
+        .id = MSG_ID_DEVICE_CONFIG,                \
+        .data = {                                  \
+            .device = {                            \
+                .type = DEVICE_SENSOR,             \
+                .enabled = true,                   \
+                .config = {                        \
+                    .sensor = MOCK_SENSOR_CONFIG}} \
+        }                                          \
+    }
+
+#define MOCK_GATEWAY_CONFIG                 \
+    {                                       \
+        .id = 1234,                         \
+        .data_out = {                       \
+            .enabled = false,               \
+            .path = "data_out_path",        \
+            .interval = 10000,              \
+            .format = GATEWAY_JSON_FORMAT}, \
+        .health_out = {                     \
+            .enabled = false,               \
+            .path = "health_out_path",      \
+            .interval = 5000,               \
+            .format = GATEWAY_JSON_FORMAT   \
+        }                                   \
+    }
+
+#define MOCK_GATEWAY_MESSAGE                         \
+    {                                                \
+        .id = MSG_ID_DEVICE_CONFIG,                  \
+        .data = {                                    \
+            .device = {                              \
+                .type = DEVICE_GATEWAY,              \
+                .enabled = true,                     \
+                .config = {                          \
+                    .gateway = MOCK_GATEWAY_CONFIG}} \
+        }                                            \
+    }
+
+#define MOCK_DATALOGGER_CONFIG    \
+    {                             \
+        .id = 1234,               \
+        .interval = 60,           \
+        .file_path = "/file.dat", \
+        .file_max_size = 1023     \
+    }
+
+#define MOCK_DATALOGGER_MESSAGE                         \
+    {                                                   \
+        .id = MSG_ID_DEVICE_CONFIG,                     \
+        .data = {                                       \
+            .device = {                                 \
+                .type = DEVICE_DATALOGGER,              \
+                .enabled = true,                        \
+                .config = {                             \
+                    .datalogger = MOCK_GATEWAY_CONFIG}} \
+        }                                               \
     }
 
 #endif //!__DEVICE_MOCKS__H__
