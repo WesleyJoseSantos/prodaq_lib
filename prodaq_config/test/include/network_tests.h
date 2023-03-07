da56e7f0cbeee45ce0cfa01550baa8ceafc35ba4
WESLEY JOSE SANTOS <BINARY_QUANTUM@outlook.com>
2023-03-06 21:31:16 -0300

    feat: create network tests prototypes

---
+++ prodaq_config/test/include/network_tests.h
@@ -0,0 +1,27 @@
+/**
+ * @file network_tests.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef __NETWORK_TESTS__H__
+#define __NETWORK_TESTS__H__
+
+void ble_status_from_json_test(void);
+void ble_status_to_json_test(void);
+
+void network_status_from_json_test(void);
+void network_status_to_json_test(void);
+
+void tcp_status_from_json_test(void);
+void tcp_status_to_json_test(void);
+
+void wifi_status_from_json_test(void);
+void wifi_status_to_json_test(void);
+
+#endif  //!__NETWORK_TESTS__H__
