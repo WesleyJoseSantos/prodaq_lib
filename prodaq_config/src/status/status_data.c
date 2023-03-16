7bfa8ac2b03f9e9852ef2c21dfdba7168f5a1595
Wesley Santos <BINARY_QUANTUM@outlook.com>
2023-03-16 08:58:49 -0300

    feat: implement status_data

--- prodaq_config/src/status/status_data.c
+++ prodaq_config/src/status/status_data.c
@@ -10,3 +10,41 @@
  */
 
 #include "status/status_data.h"
+
+prodaq_err_t status_data_from_json(PRODAQ_FROM_JSON_PARAMETERS(status_data_t), status_type_t type)
+{
+    PRODAQ_JSON_CHECK_PARAMETERS();
+    switch (type)
+    {
+    case STATUS_ETHERNET:      
+        return ethernet_info_from_json(json, &ptr->ethernet);
+    case STATUS_WIFI:      
+        return wifi_info_from_json(json, &ptr->wifi);
+    case STATUS_BLE:      
+        return ble_info_from_json(json, &ptr->ble);
+    case STATUS_NETWORK:      
+        return network_info_from_json(json, &ptr->network);
+    default:
+        return PRODAQ_ERR_INVALID_TYPE;
+    }
+    return PRODAQ_OK;
+}
+
+prodaq_err_t status_data_to_json(PRODAQ_TO_JSON_PARAMETERS(status_data_t), status_type_t type)
+{
+    PRODAQ_JSON_CHECK_PARAMETERS();
+    switch (type)
+    {
+    case STATUS_ETHERNET:      
+        return ethernet_info_to_json(&ptr->ethernet, json);
+    case STATUS_WIFI:      
+        return wifi_info_to_json(&ptr->wifi, json);
+    case STATUS_BLE:      
+        return ble_info_to_json(&ptr->ble, json);
+    case STATUS_NETWORK:      
+        return network_info_to_json(&ptr->network, json);
+    default:
+        return PRODAQ_ERR_INVALID_TYPE;
+    }
+    return PRODAQ_OK;
+}
