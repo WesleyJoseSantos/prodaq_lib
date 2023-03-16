354e1563086b09f934bdb8aae9d457ca1c55b4f5
Wesley Santos <BINARY_QUANTUM@outlook.com>
2023-03-16 08:54:20 -0300

    feat: create status_message

---
+++ prodaq_config/src/status/status_message.c
@@ -0,0 +1,12 @@
+/**
+ * @file status_message.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "status/status_message.h"
